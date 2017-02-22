34fa7a80737c94cf7ea5c7607e49c5de1b5c8a29
zhangwenfeng <zhangwenfeng@piaoshifu.cn>
2017-02-22 16:55:49 +0800

    m

--- src/Zudar/ChinaID/ID/IDInterface.php
+++ src/Zudar/ChinaID/ID/IDInterface.php
@@ -6,52 +6,59 @@ interface IDInterface
 
     /**
      * 是否符合身份证规则
-     * 
+     *
      * @return bool
      */
     public function isValidate();
 
     /**
      * 获取地区
-     * 
+     *
      * @return string
      */
     public function getArea();
 
     /**
      * 获取性别
-     * 
+     *
      * @return int
      */
     public function getGender();
 
     /**
      * 获取生日
-     * 
+     *
      * @return int
      */
     public function getBirthday();
 
     /**
      * 生成身份证号码
-     * 
+     *
      * @return string
      */
     public function generate();
 
     /**
      * 获取地区码
-     * 
+     *
      * @return int
      */
     public function getAreaCode();
 
     /**
      * 获取生日码
-     * 
+     *
      * @return int
      */
     public function getBirthdayCode();
+
+    /**
+     * 设置身份证号码
+     * 
+     * @param unknown $idNum
+     */
+    public function setIdNum($idNum);
 }
 
 ?>
